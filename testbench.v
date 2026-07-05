5a452550d0025ebef2619d262b29a6ca65c1d5b1
Shyam Rajaram <83317160+ShyamRajCool@users.noreply.github.com>
2026-07-05 17:26:43 -0600

    Create testbench.v
    Testbench temporary save state (not working)

---
+++ testbench.v
@@ -0,0 +1,53 @@
+`timescale 1ns / 100 ps
+
+module tb_clock_gen;
+
+    reg[7:0] input_MISO, input_POMI;
+    reg reset, data_in, CPOL, CPHA;
+    reg clk;
+
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
+
+    wire[7:0] MOSI, PIMO;
+    wire cs, done, divider;
+    wire[3:0] clock_counter;
+
+    spi_main uut(
+        .MISO(input_MISO), 
+        .POMI(input_POMI), 
+        .reset(reset),
+        .clk_in(clk),
+        .data_in(data_in), 
+        .CPOL(CPOL),
+        .CPHA(CPHA),
+        .MOSI(MOSI), 
+        .PIMO(PIMO), 
+        .cs(cs), 
+        .done(done), 
+        .divider(divider)
+    );
+
+
+    initial begin
+        $dumpfile("output.vcd");
+        $dumpvars(0, tb_clock_gen);
+
+        reset = 1'b1;
+        #10;
+
+        reset = 1'b0;
+        #10;
+        data_in = 1'b1;
+        CPOL = 1'b0;
+        CPHA = 1'b0;
+
+        input_MISO = 8'b10000001;
+        input_POMI = 8'b10000011;
+
+        #200;
+        $finish;
+    end
+endmodule
